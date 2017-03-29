d50409ac983b3d1c7882a76ba31864106a86ba7e
benjammin12 <benxerri@gmail.com>
2017-03-29 00:33:32 -0400

    zzz

--- main.go
+++ main.go
@@ -4,13 +4,34 @@ import (
 	"text/template"
 	"os"
 	"fmt"
+	"log"
 )
 
 var details *template.Template //define a global pointer to a template
+var tpl *template.Template
 
 func init() {
 	details = template.Must(template.ParseGlob("templates/*")) //gives you back a pointer to a template and checks for errors
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
+type course struct {
+	Number string
+	Name   string
+	Units  string
+}
+
+type semester struct {
+	Term    string
+	Courses []course
+}
+
+type year struct {
+	AcaYear string
+	Fall    semester
+	Spring  semester
+	Summer  semester
+}
+
 
 type Person struct {
 	Name string //these must be capital names, which make them public for other classes to see
@@ -40,6 +61,52 @@ func main() {
 		"Accord": "Honda",
 		"Civic": "Honda",
 		"CRV": "Honda",
+	}
+
+	years := []year{
+		year{
+			AcaYear: "2020-2021",
+			Fall: semester{
+				Term: "Fall",
+				Courses: []course{
+					course{"CSCI-40", "Introduction to Programming in Go", "4"},
+					course{"CSCI-130", "Introduction to Web Programming with Go", "4"},
+					course{"CSCI-140", "Mobile Apps Using Go", "4"},
+				},
+			},
+			Spring: semester{
+				Term: "Spring",
+				Courses: []course{
+					course{"CSCI-50", "Advanced Go", "5"},
+					course{"CSCI-190", "Advanced Web Programming with Go", "5"},
+					course{"CSCI-191", "Advanced Mobile Apps With Go", "5"},
+				},
+			},
+		},
+		year{
+			AcaYear: "2021-2022",
+			Fall: semester{
+				Term: "Fall",
+				Courses: []course{
+					course{"CSCI-40", "Introduction to Programming in Go", "4"},
+					course{"CSCI-130", "Introduction to Web Programming with Go", "4"},
+					course{"CSCI-140", "Mobile Apps Using Go", "4"},
+				},
+			},
+			Spring: semester{
+				Term: "Spring",
+				Courses: []course{
+					course{"CSCI-50", "Advanced Go", "5"},
+					course{"CSCI-190", "Advanced Web Programming with Go", "5"},
+					course{"CSCI-191", "Advanced Mobile Apps With Go", "5"},
+				},
+			},
+		},
+	}
+
+	err := tpl.Execute(os.Stdout, years)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	details.ExecuteTemplate(os.Stdout,"index.html", profess)
